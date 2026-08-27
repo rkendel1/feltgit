47f9f75192e678fcc5a15c5114182f7ed082c03c
Elijah Newren <newren@gmail.com>
2026-08-27 01:02:14 +0000

    commit: refuse partial commits during conflict resolution
    Similar to the previous commit, just as `git commit --amend` is a
    foot-gun during conflict resolution, so is a partial commit (`git commit
    <paths>`).  Recording a conflict resolution is about capturing the state
    of the entire tree on top of HEAD, not a subset of paths.  For many
    years we have rejected partial commits in the middle of
      - a merge
      - a cherry-pick
      - a rebase that stopped at a pick
    
    but, just like amending, this was never extended to the other
    operations that can also leave conflicts to resolve:
      - an `am` operation
      - a revert
      - a rebase that stopped for conflict resolution
    
    Reuse sequencer_ongoing_operation(), introduced for the analogous
    `--amend` check, to detect all of these and refuse the partial commit.
    
    Signed-off-by: Elijah Newren <newren@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/commit.c
+++ builtin/commit.c
@@ -515,13 +515,21 @@ static const char *prepare_index(const char **argv, const char *prefix,
 	 */
 	commit_style = COMMIT_PARTIAL;
 
-	if (whence != FROM_COMMIT) {
-		if (whence == FROM_MERGE)
-			die(_("cannot do a partial commit during a merge."));
-		else if (is_from_cherry_pick(whence))
-			die(_("cannot do a partial commit during a cherry-pick."));
-		else if (is_from_rebase(whence))
-			die(_("cannot do a partial commit while resolving a commit that became empty."));
+	switch (sequencer_ongoing_operation(the_repository, whence)) {
+	case ONGOING_NONE:
+		break;
+	case ONGOING_MERGE:
+		die(_("cannot do a partial commit during a merge."));
+	case ONGOING_CHERRY_PICK:
+		die(_("cannot do a partial commit during a cherry-pick."));
+	case ONGOING_REBASE_EMPTY:
+		die(_("cannot do a partial commit while resolving a commit that became empty."));
+	case ONGOING_REVERT:
+		die(_("cannot do a partial commit during a revert."));
+	case ONGOING_AM:
+		die(_("cannot do a partial commit during an am session."));
+	case ONGOING_REBASE_CONFLICT:
+		die(_("cannot do a partial commit while resolving conflicts during a rebase."));
 	}
 
 	if (list_paths(&partial, !current_head ? NULL : "HEAD", &pathspec))
